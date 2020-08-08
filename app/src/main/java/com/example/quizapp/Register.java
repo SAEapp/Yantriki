8aac28119abedf130095fd825560cbc8ecd3e147
Anant Shukla <anantshukla1402@gmail.com>
2020-08-08 19:14:49 +0530

    SplashScreen added

--- app/src/main/java/com/example/quizapp/Register.java
+++ app/src/main/java/com/example/quizapp/Register.java
@@ -121,6 +121,7 @@ public class Register extends AppCompatActivity {
                                         user.put("email",email);
                                         user.put("phone",phone);
                                         user.put("total_score","0");
+
                                         documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                                             @Override
                                             public void onSuccess(Void aVoid) {
