050c4480202d14ccfd66d83042decc92ca95252e
Vibhanshu <VibhanshuV00@gmail.com>
2020-08-09 21:37:58 +0530

    Some Things Added 1

--- app/src/main/java/com/example/quizapp/QuestionsActivity.java
+++ app/src/main/java/com/example/quizapp/QuestionsActivity.java
@@ -1,6 +1,5 @@
 package com.example.quizapp;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,9 +16,6 @@ import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
-
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
 
 import android.os.Build;
 import android.os.Bundle;
